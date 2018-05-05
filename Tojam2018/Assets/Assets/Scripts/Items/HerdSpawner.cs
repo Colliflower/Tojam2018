9a9c52a56ac00692531b975c4dc7fb3c0b841685
Liam Jackson <lijax@hotmail.com>
2018-05-05 15:43:20 -0400

    Added goat related files

---
+++ Tojam2018/Assets/Assets/Scripts/Items/HerdSpawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HerdSpawner : MonoBehaviour {
+	public GameObject goat;
+	public GameObject cam;
+	public int numGoats;
+	private GameObject[] goats;
+	private int counter;
+	public float xOffset;
+	public float zOffset;
+	public float speed;
+	private float timer;
+	public float step;
+	private bool toDestroy;
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		toDestroy = false;
+		goats = new GameObject[numGoats];
+		for (int i = 0; i < 2; i++) {
+			goats[counter] = Instantiate(goat,new Vector3(transform.position.x-i*xOffset+xOffset*0.5f,0,transform.position.z), Quaternion.identity);
+			goats[counter].GetComponent<GoatController> ().bounceScale = Random.Range (1, 4);
+			goats[counter].GetComponent<GoatController> ().step = Random.Range (0.1f, 0.3f);
+			goats[counter].GetComponent<GoatController> ().timer = Random.Range (0.0f, Mathf.PI);
+			goats[counter].GetComponent<GoatController> ().speed = speed;
+			counter++;
+
+		}
+		for (int i = 0; i < 3; i++) {
+			goats[counter] = Instantiate(goat,new Vector3(transform.position.x-i*xOffset,0,transform.position.z-zOffset), Quaternion.identity);
+			goats[counter].GetComponent<GoatController> ().bounceScale = Random.Range (1, 4);
+			goats[counter].GetComponent<GoatController> ().step = Random.Range (0.1f, 0.3f);
+			goats[counter].GetComponent<GoatController> ().timer = Random.Range (0.0f, Mathf.PI);
+			goats[counter].GetComponent<GoatController> ().speed = speed;
+			counter++;
+		}
+		for (int i = 0; i < 2; i++) {
+			goats[counter] = Instantiate(goat,new Vector3(transform.position.x-i*xOffset+xOffset*0.5f,0,transform.position.z-zOffset*2), Quaternion.identity);
+			goats[counter].GetComponent<GoatController> ().bounceScale = Random.Range (1, 4);
+			goats[counter].GetComponent<GoatController> ().step = Random.Range (0.1f, 0.3f);
+			goats[counter].GetComponent<GoatController> ().timer = Random.Range (0.0f, Mathf.PI);
+			goats[counter].GetComponent<GoatController> ().speed = speed;
+			counter++;
+		}
+	}
+	void FixedUpdate(){
+		if (!toDestroy) {
+			timer += step;
+			transform.position = new Vector3 (transform.position.x, 0, transform.position.z + speed * Time.fixedDeltaTime);
+			if (timer >= 100) {
+				for (int i = 0; i < numGoats; i++) {
+					goats [i].GetComponent<GoatController> ().bounceScale = 20;
+					//goats [i].GetComponent<GoatController> ().step = 0.01f;
+				}
+				toDestroy = true;
+			}
+		} else {
+			timer += step;
+			if (timer >= 200) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}

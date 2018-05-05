fb105dd1b378368fd48aff9e934fcd3c78ca3c40
andriyDev <andriydzikh@gmail.com>
2018-05-05 00:14:15 -0400

    Setup the code to use the free direction item usage.

--- Tojam2018/Assets/Assets/Scripts/PlayerController.cs
+++ Tojam2018/Assets/Assets/Scripts/PlayerController.cs
@@ -92,7 +92,7 @@ public class PlayerController : MonoBehaviour {
         }
 
         Animator anim = GetComponent<Animator>();
-        anim.SetFloat("Speed", finalMove.magnitude / 5);
+        anim.SetFloat("Speed", finalMove.magnitude);
 
         lastFrameVelocity = finalMove;
     }
@@ -137,11 +137,12 @@ public class PlayerController : MonoBehaviour {
         }
         else
         {
-            float absX = Mathf.Abs(previousThrowVector.x);
-            float absY = Mathf.Abs(previousThrowVector.y);
+            // float absX = Mathf.Abs(previousThrowVector.x);
+            // float absY = Mathf.Abs(previousThrowVector.y);
             bool FiredAndCompleted = false;
             if (previousThrowVector.magnitude >= throwDeadzone)
             {
+                /* 4 Directions
                 if (absX >= absY && previousThrowVector.x < 0)
                 {
                     if (throwVector.magnitude < throwDeadzone) { FiredAndCompleted = storedItem.FireLeft(); } else { storedItem.AimLeft(); }
@@ -158,8 +159,8 @@ public class PlayerController : MonoBehaviour {
                 {
                     if (throwVector.magnitude < throwDeadzone) { FiredAndCompleted = storedItem.FireDown(); } else { storedItem.AimDown(); }
                 }
-                // Alternative: Instead of having cardinal directions, just use an absolute direction.
-                // if (throwVector.magnitude < throwDeadzone) { FiredAndCompleted = storedItem.Fire(previousThrowVector); } else { storedItem.Aim(previousThrowVector); }
+                */
+                if (throwVector.magnitude < throwDeadzone) { FiredAndCompleted = storedItem.Fire(previousThrowVector); } else { storedItem.Aim(previousThrowVector); }
             }
 
             previousThrowVector = throwVector;

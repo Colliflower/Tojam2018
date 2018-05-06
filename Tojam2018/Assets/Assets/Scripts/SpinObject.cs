d46541677c6aea9ffe8f6cc92b17dbd5066f4826
andriyDev <andriydzikh@gmail.com>
2018-05-05 20:11:54 -0400

    Working on getting the waterbottle in.

---
+++ Tojam2018/Assets/Assets/Scripts/SpinObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        //transform.rotation
+    }
+}
